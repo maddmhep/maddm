1d58ac324600d28170bae78235344c322f6a1475
olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2016-04-18 09:44:16 +0100

    addopt the plugin approach for maddm

--- maddm_interface.py
+++ maddm_interface.py
@@ -1,1 +1,7 @@
-"template for now"
+import madgraph.interface.master_interface as master_interface
+
+class MadDM_interface(master_interface.MasterCmd):
+
+    def do_helloworld(self, line):
+        """print hello world"""
+        print "hello world"
