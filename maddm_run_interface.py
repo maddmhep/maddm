03f9f328bea0367099c4afce76e3fd67b34f428f
Federico Ambrogi fambrogi@cern.ch <>
2018-01-30 12:57:25 +0100

    Updated Fermi lim class and added Fermi_Data dir

--- maddm_run_interface.py
+++ maddm_run_interface.py
@@ -233,6 +233,213 @@ class PPPC_Spectra:
         return combined 
 
 
+    # FF this function extracts the values of the spectra interpolated linearly between two values mdm_1 and mdm_2                                                                 
+    # mdm is the DM candidate mass, spectrum is gammas, positron etc, channel is the SM annihilation e.g. bbar, hh etc.                                                            
+    # FF remember to CHECK if it works when min and max are the same values, i.e. exactly for a value in the Masses lists!!!                                                       
+    def interpolate_spectra(self, sp_dic, spectrum , channel):
+        print 'FF I am interpolating'
+        mdm = self.param_card.get_value('mass', self.proc_characteristics['dm_candidate'][0])
+        M   = sp_dic['Masses']
+        dm_min =  M[M >= mdm].min()  # extracting lower mass limit to interpolate from                                                                                             
+        dm_max =  M[M <= mdm].max()  # extracting upper mass limit to interpolate from                                                                                             
+
+        spec_1 = Dic[spectrum][ str(dm_min) ][channel]
+        spec_2 = Dic[spectrum][ str(dm_max) ][channel]
+
+        interpolated = []
+        for x in range(len(spec_1)): # the spectrum values are ordered as 'x' vaues extracted from the Log[10,x] in the PPPC Tables                                                
+               interp_function = interp1d([dm_min, dm_max], [spec_1[x],spec_2[x]] )
+               value =  interp_function(mdm)
+               interpolated.append(value)
+
+        return interpolated
+
+
+
+
+class Fermi_bounds:
+
+    def __init__(self):
+
+        self.sigmav   = 0
+        self.p_value = 0
+        self.nBin = 24
+        self.j0 = 3.086e21 # convention spectra    
+        self.dw_in = self.dw_dic()
+        self.dSph_jfac_file =  pjoin(MDMDIR, 'Fermi_Data', 'Jfactors.dat')
+        self.dSph_ll_files_path = pjoin(MDMDIR, 'Fermi_Data', 'likelihoods')
+        self.dwarves_list = ['coma_berenices', 'draco', 'segue_1', 'ursa_major_II', 'ursa_minor', 'reticulum_II' ]
+        self.dwarveslist_all = self.extract_dwarveslist() 
+
+
+# This function reads the list of dwarves from the Jfactor.dat file
+    def extract_dwarveslist(self):
+        dwarflist_all = []
+        op = open(self.dSph_jfac_file,'r')
+        for line in [ line for line in op.readlines() if 'Row' in line]:
+            dwarveslist_all.append (line.split(' ')[3].replace('\n',''))
+        return dwarflist_all
+
+    def dw_dic(self):
+        
+        dSph_jfac_file, dSph_ll_files_path, nBin = self.dSph_jfac_file , self.dSph_ll_files_path , self.nBin
+        dwarflist_all = extract_dwarveslist(dSph_jfac_file) # create the list of dwarves reading the Jfactor dat file                                                          
+        dwarf_list = self.dwarf_list
+
+        dSph_ll_files = [ 'like_' + dwarf + '.txt' for dwarf in dwarflist_all ]
+
+        jbar_dSph = np.loadtxt(dSph_jfac_file, unpack=True)[3]
+        jbar_dSph_error = np.loadtxt(dSph_jfac_file, unpack=True)[4]
+
+        dw_dic_coll = {}
+
+        for dwarf in dwarflist_all:
+            for D in dwarf_list:
+               if D == dwarf:
+                  elem = 'like_'+str(dwarf)+'.txt'
+
+                  if elem in dSph_ll_files:
+                     dwindex = dSph_ll_files.index(elem)
+                     likefile = dSph_ll_files[dwindex]
+                     data = np.loadtxt(dSph_ll_files_path+likefile, unpack=True)
+                     efluxes = (1e-3*data[2]).reshape(nBin,-1) # convert flux list to 2D list of fluxes for all ebins, convert to GeV                                               
+                     logLikes = data[3].reshape(nBin,-1)
+
+                     likes = [ interp1d(f,l,bounds_error=False, fill_value=-1e5) for f,l in zip(efluxes,logLikes) ]
+
+                     ll_null = 0.0
+                     for like in likes:
+                         ll_null+=like(0)
+
+                     jfact = jbar_dSph[dwindex]
+                     jfacterr = jbar_dSph_error[dwindex]
+
+                     dict = { 'Jfac': jfact,
+                              'Jfac_err': jfacterr,
+                              'll_null': ll_null,
+                              'likelihood': likes }
+
+                     dw_dic_coll[dwarf] = dict
+
+        #print "dwarfs included:", dw_dic_coll.keys()
+        return dw_dic_coll
+
+    def eflux(self,spectrum, emin=1e2, emax=1e5, quiet=False):
+       """ Integrate a generic spectrum, multiplied by E, to get the energy flux.                                                                                             
+       """
+       espectrum = lambda e: spectrum(e)*e
+       tol = min(espectrum(emin),espectrum(emax))*1e-10
+       try:
+          return quad(espectrum,emin,emax,epsabs=tol,full_output=True)[0]
+       except Exception, msg:
+          logger.info('Numerical error "%s" when calculating integral flux.' % msg)
+       return np.nan
+
+    def marg_like_dw(self,pred,marginalize):
+      
+        j0, nBin = self.j0 , self.nBin
+
+        dw_in_i = self.dw_in
+        j,jerr,like_inter = dw_in_i['Jfac'], dw_in_i['Jfac_err'], dw_in_i['likelihood']
+
+       
+        # function to be minimized:                                                                                                                                           
+        def chi2min(x):
+
+            flux_like = 0.0
+
+            for i in range(nBin):
+
+                like_i = like_inter[i]
+                pred_i = pred[i]
+                flux = pred_i*10**(j+x*jerr)/j0
+                flux_like += like_i(flux)
+            jfac_like = - 0.5*x**2
+            return -2.0*(flux_like+jfac_like)
+
+        if marginalize:
+           res = minimize_scalar(chi2min)
+           jsigma = res.x
+           ll_max = -0.5*res.fun
+        else:
+           ll_max = -0.5*chi2min(0.0)
+           jsigma = 0.0
+
+        return ll_max, jsigma
+
+
+    def res_tot_dw(self,pred,marginalize):
+
+        dw_in = self.dw_in
+        ll_tot = 0.0
+        ll_null = 0.0
+        j_factors = {}
+
+        for k,v in dw_in.iteritems():
+            marg_like = marg_like_dw(v,pred,marginalize)
+            ll_tot += marg_like[0]
+            ll_null += v['ll_null']
+            j_factors[k] = marg_like[1]
+
+        pval = compute_pvalue(ll_tot,ll_null)
+        return ll_tot, ll_null, pval, j_factors
+
+
+    def Fermi_sigmav_lim(self, mDM, x = '' , dndlogx = '' , marginalize = True, calc_p_value = True):
+        np.seterr(divide='ignore', invalid='ignore')   # Keep numpy from complaining about dN/dE = 0...                                                                       
+
+        j0 , nBin = self.j0 , self.nBin # convention spectra                                                                                                                     
+        sigmav0 = 1e-26
+
+        emins = [0.5, 0.666760716, 0.8891397050000001, 1.1856868500000002, 1.5811388300000002, 2.10848252, 2.81170663, 3.7494710500000004, 5.0, 6.667607159999999, 
+                 8.89139705 , 11.856868500000001, 15.8113883, 21.0848252, 28.117066299999998, 37.494710500000004, 50.0, 66.6760716, 88.91397049999999, 
+                 118.568685, 158.11388300000002, 210.848252, 281.170663, 374.94710499999997]
+        emaxs = [0.666760716, 0.8891397050000001, 1.1856868500000002, 1.5811388300000002, 2.10848252, 2.81170663, 3.7494710500000004, 5.0, 6.667607159999999, 
+                 8.89139705, 11.856868500000001, 15.8113883, 21.0848252, 28.117066299999998, 37.494710500000004, 50.0, 66.6760716, 88.91397049999999, 
+                 118.568685, 158.11388300000002, 210.848252, 281.170663, 374.94710499999997, 500.0]
+
+    # Load the spectrum E, dN/dE                                                                                                                                              
+    #energy,dnde = np.loadtxt(specfile,unpack=True)                                                                                                                           
+    #Load the spectrum in logx, dN/dlogx -> contained in the dictionary of dictionaries                                                                                       
+        logx = np.log10(x)
+        mDM = eval(mDM) # must be in GeV                                                                                                                                      
+
+        energy = mDM*10**logx
+
+        dnde = (dndlogx/(mDM*10**logx*2.30259))
+        log_energy = np.log10(energy)
+        log_dnde = np.log10(dnde)
+        log_interp = interp1d(log_energy,log_dnde)
+        spectrum = lambda e: np.nan_to_num(10**( log_interp(np.log10(e)) )) if (e <= energy.max() and e >= energy.min()) else 0.0
+
+        pred = np.array([eflux(spectrum,e1,e2)/mDM**2*sigmav0*j0/(8.*pi) for e1,e2 in zip(emins,emaxs)])
+
+        CL_VAL= 0.95
+
+        def find_sigmav(x,pred,dw_in,marginalize):
+
+            pred_sigma = pred*np.power(10, (-28.0+2.0*x))/sigmav0                                 
+            pvalue = res_tot_dw(dw_in,pred_sigma,marginalize)[2]
+
+            pvalue=np.sqrt(pvalue**2+(x**2)**0.01/0.001) if pvalue==1 else pvalue
+            return (pvalue-CL_VAL)**2
+
+        find_sig = lambda x: find_sigmav(x,pred,dw_in,marginalize)
+        res  = minimize_scalar(find_sig, method='brent'  ,  bounds = (0,1) )                                                                                       
+
+        sigmav_ul   = 10**(-28+2.0*res.x)
+        p_value = -1
+
+        if calc_p_value == True:
+           pred_sigma = pred*sigmav_ul/sigmav0
+           result  = res_tot_dw(dw_in,pred_sigma,marginalize)
+           p_value = result[2]
+
+        self.sigmav  = sigmav_ul
+        self.p_value = p_value 
+
+
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -987,15 +1194,7 @@ class MADDMRunCmd(cmd.CmdShell):
             
             return phi
 
-    # FF check if these are already imported somewhere else?
-    #import numpy as np
-    #import scipy
-    #from scipy.interpolate import interp1d
-    #from scipy.integrate import quad
-    #from scipy.optimize import minimize_scalar
-    #from scipy.special import gammainc
-    #from math import pi
-    
+    '''
     nBin = 24
     j0 = 3.086e21 # convention spectra
 
@@ -1121,7 +1320,7 @@ class MADDMRunCmd(cmd.CmdShell):
     
         pval = compute_pvalue(ll_tot,ll_null)
         return ll_tot, ll_null, pval, j_factors
-
+   
 
     def Fermi_sigmav_lim(mDM, x = '' , dndlogx = '' , dw_in = '' , marginalize = True, calc_p_value = True):
         np.seterr(divide='ignore', invalid='ignore')   # Keep numpy from complaining about dN/dE = 0...
@@ -1131,9 +1330,9 @@ class MADDMRunCmd(cmd.CmdShell):
         j0 = 3.086e21 # convention spectra
         sigmav0 = 1e-26
 
-        emins = [0.5, 0.666760716, 0.8891397050000001, 1.1856868500000002, 1.5811388300000002, 2.10848252, 2.81170663, 3.7494710500000004, 5.0, 6.667607159999999, 8.89139705, 11.856868500000001, 15.8113883, 21.0848252, 28.117066299999998, 37.494710500000004, 50.0, 66.6760716, 88.91397049999999, 118.568685, 158.11388300000002, 210.848252, 281.170663, 374.94710499999997]
-        emaxs = [0.666760716, 0.8891397050000001, 1.1856868500000002, 1.5811388300000002, 2.10848252, 2.81170663, 3.7494710500000004, 5.0, 6.667607159999999, 8.89139705, 11.856868500000001, 15.8113883, 21.0848252, 28.117066299999998, 37.494710500000004, 50.0, 66.6760716, 88.91397049999999, 118.568685, 158.11388300000002, 210.848252, 281.170663, 374.94710499999997, 500.0]
-
+        #emins = [0.5, 0.666760716, 0.8891397050000001, 1.1856868500000002, 1.5811388300000002, 2.10848252, 2.81170663, 3.7494710500000004, 5.0, 6.667607159999999, 8.89139705, 11.856868500000001, 15.8113883, 21.0848252, 28.117066299999998, 37.494710500000004, 50.0, 66.6760716, 88.91397049999999, 118.568685, 158.11388300000002, 210.848252, 281.170663, 374.94710499999997]
+        #emaxs = [0.666760716, 0.8891397050000001, 1.1856868500000002, 1.5811388300000002, 2.10848252, 2.81170663, 3.7494710500000004, 5.0, 6.667607159999999, 8.89139705, 11.856868500000001, 15.8113883, 21.0848252, 28.117066299999998, 37.494710500000004, 50.0, 66.6760716, 88.91397049999999, 118.568685, 158.11388300000002, 210.848252, 281.170663, 374.94710499999997, 500.0]
+   
     # Load the spectrum E, dN/dE
     #energy,dnde = np.loadtxt(specfile,unpack=True)
     #Load the spectrum in logx, dN/dlogx -> contained in the dictionary of dictionaries
@@ -1172,7 +1371,9 @@ class MADDMRunCmd(cmd.CmdShell):
            p_value = result[2]
 
         return [sigmav_ul , p_value]
-        
+    ''' 
+
+    
     def print_results(self):
         """ print the latest results """
         #omega_min = 0
