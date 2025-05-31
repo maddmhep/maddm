4fce624644659500f554e9f3efc397ee9a24b037
Gian Marco Lucchetti <gianmarcolucchetti@Mac.station>
2025-05-31 12:08:12 +0200

    Update Earth velocity

--- python_templates/electron_recoil_signal.f
+++ python_templates/electron_recoil_signal.f
@@ -224,7 +224,7 @@ c       Setup the array of day values. Each day value is in the centre of the bi
 
 c       Write the results
         open(3,file='./output/dRdE_e_recoil.dat',status='unknown') 
-        open(4,file='./output/rate_vs_time_e_recoil.dat',status='unknown')
+c        open(4,file='./output/rate_vs_time_e_recoil.dat',status='unknown')
 c        open(5,file='./output/tot_rate.dat',status='unknown')
         open(6,file='./output/dRdS2_Xenon10_e_recoil.dat',status='unknown')
         open(7,file='./output/dRdiS2_Xenon1T_e_recoil.dat',status='unknown')
@@ -246,12 +246,12 @@ c       ================================================================
 c       Recoil Rate R        ./Output/rate_vs_time.dat
 c       day(i), rate_(i)
 c       ================================================================
-        write(4,*) '## Rates[events/kg/month]'
-        write(4,*) '## unsmeared  ##'
+c        write(4,*) '## Rates[events/kg/month]'
+c        write(4,*) '## unsmeared  ##'
 
-        do id = 1, day_bins
-            write(4,*) daymid(id), rate_vs_time(id)
-        enddo
+c        do id = 1, day_bins
+c            write(4,*) daymid(id), rate_vs_time(id)
+c        enddo
 
 c       Writing out the total rate. R = dN/dt(years)
 c       ================================================================
@@ -280,7 +280,7 @@ c       Writing out the expected numer of events for Xenon1T, using the center v
         enddo
         
         close(3)
-        close(4)
+c        close(4)
 c        close(5)
         close(6)
         close(7)
@@ -301,7 +301,7 @@ c        close(5)
 
         double precision M_dm, M_e, E_binding
         double precision ioniz_amplitude(gridsize_k,gridsize_q)
-        double precision get_dRdlogE, dday, v_earth
+        double precision get_dRdlogE, dday
         double precision dRdlogEdday_kg_day(gridsize_k,day_bins),diff_rate_logE(gridsize_k)
         double precision rate_vs_time(day_bins),tot_rate_kg_day
 
@@ -363,12 +363,12 @@ c ------------------------------------------------------------------------------
 c       Loop over the momenta of the outgoing electron k_e.
         do ik = 1,gridsize_k
 
-c           Loop over day_bins
+c           Loop over day_bins.
             do id = 1, day_bins
 
 c               Compute the differential rate, than use to compute dR/dlogE, total rate and total events
                 dRdlogEdday_kg_day(ik,id) =
-     &             get_dRdlogE(ik,E_e(ik),E_binding,q_exc,dq,M_dm,M_e,ioniz_amplitude,v_earth(daymid(id)))
+     &             get_dRdlogE(ik,E_e(ik),E_binding,q_exc,dq,M_dm,M_e,ioniz_amplitude)
      &             *dday/365.0*daytosec
 
                 tot_rate_kg_day         = tot_rate_kg_day + dRdlogEdday_kg_day(ik,id) * dlogE(ik)
@@ -386,18 +386,17 @@ c               Compute the differential rate, than use to compute dR/dlogE, tot
 
 
 !-----------------------------------------------------------------------------------------------!
-        Function get_dRdlogE(ik, ER, Eb, q_exc, dq, M_dm, M_e, ioniz_amplitude, ve)
+        Function get_dRdlogE(ik, ER, Eb, q_exc, dq, M_dm, M_e, ioniz_amplitude)
 !-----------------------------------------------------------------------------------------------!
 !	    Calculate the double differential spectrum for DM detection.                            !
-!	    Uses the modulation information of the earth inside function v_earth.                   !
 !-----------------------------------------------------------------------------------------------!
         implicit none
 
         include '../include/maddm.inc'
 
         integer ik,iq
-        double precision get_dRdlogE, ER, Eb, ve, nDM, c
-        double precision M_dm, M_e, v0, vearth, vmin, RhoD
+        double precision get_dRdlogE, ER, Eb, nDM, c, vE
+        double precision M_dm, M_e, v0, vmin, RhoD
         double precision N_events, eta, r_kin, kNorm, vesc, const_integral
         double precision q_exc(gridsize_q+1), dq(gridsize_q)
 
@@ -409,9 +408,14 @@ c       Parameters and variables
         c       = 29979245800.d0              ! cm sec**(-1)
         v0      = vMP*(km/sec)                ! Most Probable velocity of WIMPs in DM Halo
         vesc    = vescape*(km/sec)            ! Escape velocity of a WIMP from the Galactic Halo
-        vearth  = ve*(km/sec)                 ! Velocity of the Earth, taken from the v_earth function (cm/s)
         RhoD    = rhoDM*GeV*cs**(-2)*cm**(-3) ! Density of Dark Matter in our local part of the Galaxy
         nDM = RhoD / M_dm * cm**(-3)          ! Number density of DM [cm**(-3)]
+
+c       Module of the velocity of the Earth respect to the centre of the Galaxy.
+c       coord: (r,phi,theta). 
+        vE = sqrt(((vearth_r+vSun_r)**2 +
+     &             (vearth_phi+vSun_phi+vMP)**2 +
+     &             (vearth_theta+vSun_theta)**2)) * (km/sec)
         
         const_integral = nDM/(128.d0*pi*((M_dm*GeVtoKg)**2)*((M_e*GeVtoKg)**2)) ! Constant in front of the integral [cm**(-3) Kg**(-4)]
         kNorm   = (v0**3)*pi*(sqrt(pi)*erf(vesc/v0) - 2.d0*(vesc/v0)*exp(-(vesc/v0)**2)) ! Normalization factor for velocity distribution integral [cm**(3) sec**(-3)]
@@ -425,12 +429,12 @@ c           Minimum velocity required for recoil.
             vmin = ((ER+Eb)/q_exc(iq) + q_exc(iq)/(2*M_dm))*c ! [cm/sec]
 
 c           Dark matter integrated velocity distibution eta [cm**(-1) sec] (integrated also in cos(theta)) 
-            if(vmin.le.(vesc-vearth)) then
-                eta = (v0**2 * pi)/(2*vearth*kNorm) *
-     &                ((-4.d0)*exp(-(vesc/v0)**2)*vearth + sqrt(pi)*v0*(erf((vmin+vearth)/v0) - erf((vmin-vearth)/v0)))
-            else if ((vesc-vearth).le.vmin.and.vmin.le.(vesc+vearth)) then
-                eta = (v0**2 * pi)/(2*vearth*kNorm) *
-     &                ((-2.d0)*exp(-(vesc/v0)**2)*(vesc-vmin+vearth) + sqrt(pi)*v0*(erf(vesc/v0) - erf((vmin-vearth)/v0)))
+            if(vmin.le.(vesc-vE)) then
+                eta = (v0**2 * pi)/(2*vE*kNorm) *
+     &                ((-4.d0)*exp(-(vesc/v0)**2)*vE + sqrt(pi)*v0*(erf((vmin+vE)/v0) - erf((vmin-vE)/v0)))
+            else if ((vesc-vE).le.vmin.and.vmin.le.(vesc+vE)) then
+                eta = (v0**2 * pi)/(2*vE*kNorm) *
+     &                ((-2.d0)*exp(-(vesc/v0)**2)*(vesc-vmin+vE) + sqrt(pi)*v0*(erf(vesc/v0) - erf((vmin-vE)/v0)))
             else
                 eta = 0
             endif
@@ -665,53 +669,6 @@ c            https://github.com/XENON1T/s2only_data_release/blob/master/s2_binni
         enddo
 
         end subroutine get_dRdiS2_Xenon1T
-
-
-
-
-
-!-----------------------------------------------------------------------------------------!
-      Real*8 Function v_earth(days)
-!-----------------------------------------------------------------------------------------!
-!     This function calculates the velocity of the earth from the galactic velocity and   !
-!     sun proper motion.                                                                  !
-!-----------------------------------------------------------------------------------------!
-      Implicit none
-      
-      Real*8	 pi, degtorad, cosbx, cosby, cosbz, g, lambda, Lx, Ly, Lz, L0
-      Real*8	 V_E1(3), days, ecc, uearthave, u_E, vuGal(3), vuSun(3), vu_E(3)
-      
-      vuGal = (/ 0.d0, 230.d0, 0.d0 /) ! velocity of the Galactic rotation
-      vuSun = (/ 10.d0, 13.d0, 7.d0 /) ! Sun proper motion relative to nearby stars
-      
-      pi = 4.d0*Atan(1.d0)
-      degtorad = pi/180.d0      ! converting degrees to radians
-      cosbx = cos(-5.5303*degtorad) 
-      cosby = cos(59.575*degtorad)
-      cosbz = cos(29.812*degtorad)
-      
-!     Following Lewin and Smith Appendix B.
-      L0 = 13.d0*degtorad
-      Lx = 266.141d0*degtorad
-      Ly = -13.3485d0*degtorad
-      Lz = 179.3212d0*degtorad
-      
-      g = degtorad*357.528d0 + 0.9856003d0*days*degtorad 
-      lambda = degtorad*280.46d0 + degtorad*0.9856474d0*days +
-     &     degtorad*1.915d0*sin(g) + degtorad*0.02d0*sin(2.d0*g) !galactic coordinates of the earth on each day.
-      ecc = 0.016722d0          ! Earth eccentricity.
-      uearthave = 29.79d0 
-      u_E = uearthave*(1.d0 - ecc*sin(lambda - L0)) ! Average velocity of the earth around the Sun.
-      vu_E = (/ u_E*cosbx*sin(lambda - Lx), u_E*cosby*sin(lambda - Ly), u_E*cosbz*sin(lambda - Lz) /) 
-      
-      V_E1(1) = vuGal(1) + vuSun(1) + vu_E(1)
-      V_E1(2) = vuGal(2) + vuSun(2) + vu_E(2)
-      V_E1(3) = vuGal(3) + vuSun(3) + vu_E(3)
-      
-      v_earth = sqrt(V_E1(1)**2 + V_E1(2)**2 + V_E1(3)**2)
-      
-      return
-      end 
 
 
 
