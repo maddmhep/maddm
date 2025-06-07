ce97d835c5b59f8714c12fe53ce8c6e2f38ac112
Gian Marco Lucchetti <gianmarcolucchetti@MAC-A6B2A3.station>
2025-06-07 11:23:32 +0200

    split scans and scripts files

---
+++ examples/scripts.py
@@ -0,0 +1,27 @@
+"""
+=====================
+Use MadDM as a Script
+=====================
+
+Run MadDM as a script, which is useful for batch processing or automation.
+
+1. Running MadDM as a Script
+============================
+
+To run MadDM as a script, you can simply run ``maddm.py`` by providing a text file as an argument containing the commands you want to execute. For example:
+
+.. code-block:: text
+    python bin/maddm.py run.txt
+
+where ``run.txt`` should contain the commands you want to execute, one per line, for example:
+
+.. code-block:: text
+
+    import model DMsimp_s_spin0
+    define darkmatter Xr
+    generate direct
+    output SCAN_example
+    launch SCAN_example
+    set MXd scan:range(50,700,25)
+
+"""
