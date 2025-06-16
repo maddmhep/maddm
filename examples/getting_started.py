46d8d04d9bb9fd0e3458497c44491aae580878ab
Gian Marco <lucchetti.gianmarco@gmail.com>
2025-06-16 10:28:08 +0200

    Update tutorial getting started

--- examples/getting_started.py
+++ examples/getting_started.py
@@ -20,13 +20,13 @@ First, import the model, which will be downloaded automatically from `here <http
 
 .. code-block:: text
 
-    MadDM> import model DMsimp_s_spin0
+    MadDM> import model DMsimp_s_spin1_MD
 
 Next, define the dark matter (DM) particle manually with:
 
 .. code-block:: text
 
-    MadDM> define darkmatter xd
+    MadDM> define darkmatter ~xd
 
 Generally, for DMsimp models, you can choose between Xr (real scalar DM), Xc (complex scalar DM)
 and Xd (Dirac spinor DM).
@@ -42,43 +42,50 @@ Finally, the candidate must:
 - Have some non-zero couplings,
 - Not decay into a pair of SM particles.
 
-Then, generate the direct detection processes with:
+Then, generate the direct detection (``direct``), indirect detection (``indirect``) or relic density (``relic_rensity``) processes with:
 
 .. code-block:: text
 
     MadDM> generate direct
 
-At this point, you can set the parameter values of the model, stored in ``param_card_orig.dat``, by pressing 1. This will open a vim editor where you can modify the parameters.
-Next, create the process folder. In this case, we call it ``DD_NR_spin0``, but you can choose any name you like. Then launch the process:
+If you want to add additional processes, do so by typing:
 
 .. code-block:: text
 
-    MadDM> output DD_NR_spin0
-    MadDM> launch DD_NR_spin0
+    MadDM> add indirect
+    MadDM> add relic_density
+
+At this point, you can set the parameter values of the model, stored in ``param_card_orig.dat``, by pressing 1. This will open a vim editor where you can modify the parameters.
+Next, create the process folder. In this case, we call it ``DMspin1``, but you can choose any name you like. Then launch the process:
+
+.. code-block:: text
+
+    MadDM> output DMspin1
+    MadDM> launch DMspin1
 
 Note that once the process folder has been created with the ``output`` command in a previous session, you can always
-relaunch the same process in a new session with ``launch myprocessname`` skipping the previous steps.
+relaunch the same process in a new session with ``launch myprocessname``, skipping the previous steps.
 Once you launch the process, you will be prompted a selection menu to choose which modules you want to run.
 
 .. code-block:: text
 
-    The following switches determine which programs are run:
-    /=============================================================================\\
-    | 1. Compute the Relic Density                                  relic = OFF   |
-    | 2. Compute direct detection - nucleon recoil                 direct = ON    |
-    | 3. Compute direct detection - electron recoil       direct_electron = OFF   |
-    | 4. Compute indirect detection/flux (cont spectrum)         indirect = OFF   |
-    | 5. Compute indirect detection in aX (line spectrum)        spectral = OFF   |
-    | 6. Run Multinest scan                                      nestscan = OFF   |
-    \=============================================================================/
-    You can also edit the various input cards:
+        The following switches determine which programs are run:
+    /====================== Description =======================|===================== values =====================|========== other options ===========\\
+    | 1. Compute the Relic Density                             |                relic = OFF                       |     ON                             |
+    | 2. Compute direct detection - nucleon recoil             |               direct = ON                        |     OFF                            |
+    | 3. Compute direct detection - electron recoil            |      direct_electron = ON                        |     OFF                            |
+    | 4. Compute indirect detection/flux (cont spectrum)       |             indirect = sigmav                    |     flux_source|flux_earth|OFF     |
+    | 5. Compute indirect detection in aX (line spectrum)      |             spectral = OFF                       |     Please install module          |
+    | 6. Run Multinest scan                                    |             nestscan = OFF                       |     Please install module          |
+    \\==================================================================================================================================================/
+    You can also edit the various input card:
     * Enter the name/number to open the editor
     * Enter a path to a file to replace the card
     * Enter set NAME value to change any parameter to the requested value
-    /=============================================================================\ 
-    |  6. Edit the model parameters    [param]                                    |  
-    |  7. Edit the MadDM options       [maddm]                                    |
-    \=============================================================================/
+    /=============================================================================\\ 
+    |  7. Edit the model parameters    [param]                                    |  
+    |  8. Edit the MadDM options       [maddm]                                    |
+    \\=============================================================================/
     [60s to answer]
 
 You can select the modules you want to run by entering the corresponding number or name, or by setting the
@@ -86,10 +93,7 @@ corresponding variable to ON:
 
 .. code-block:: text
 
-    MadDM> direct = ON
-
-If your model has both DM-nucleon and DM-electron interactions, you can set both ``direct`` and ``direct_electron`` modules to ON,
-and you will receive results for both types of interactions considered separately.
+    MadDM> relic = ON
 
 Some modules can be set to specific values, such as:
 
@@ -97,12 +101,63 @@ Some modules can be set to specific values, such as:
 
     MadDM> set indirect = flux_source
 
-You can set the model parameters in the ``param_card.dat`` by pressing 7, or by editing the file directly in the process folder (in this case ``DD_NR_spin0/Cards/param_card.dat``).
+You can set the model parameters in the ``param_card.dat`` by pressing 7, or by editing the file directly in the process folder (in this case ``DMspin1/Cards/param_card.dat``).
 
-You can set the MadDM parameters (such as the halo parameters, recoil energy range and much more) in the ``maddm_card.dat`` by pressing 8,
-or by editing the file directly in the process folder (in this case ``DD_NR_spin0/Cards/maddm_card.dat``).
+You can set the MadDM parameters (such as the halo parameters, recoil energy range, and much more) in the ``maddm_card.dat`` by pressing 8,
+or by editing the file directly in the process folder (in this case ``DMspin1/Cards/maddm_card.dat``).
 
-Once you are all set, press Enter to launch the process.
+Once you are all set, press Enter to launch the process. The results will be displayed in the terminal as shown below:
+
+.. code-block:: text
+
+    INFO: MadDM Results 
+    INFO: 
+    ****** Relic Density
+    OMEGA IS 11.557143 
+    INFO: Relic Density       = 1.16e+01       OVERABUNDANT       
+    INFO: x_f                 = 1.40e+01             
+    INFO: sigmav(xf)          = 8.50e-29 cm^3 s^-1   
+    INFO: xsi                 = 1.00e+00             
+    INFO:  
+    INFO: Channels contributions: 
+    INFO: xxdxxdb_ddx         : 20.08 % 
+    INFO: xxdxxdb_uux         : 20.08 % 
+    INFO: xxdxxdb_ssx         : 20.08 % 
+    INFO: xxdxxdb_ccx         : 20.08 % 
+    INFO: xxdxxdb_bbx         : 19.69 % 
+    INFO: No contribution from processes: xxdxxdb_y1y1, xxdxxdb_vevex, xxdxxdb_vmvmx, xxdxxdb_vtvtx, xxdxxdb_ttx, xxdxxdb_emep, xxdxxdb_mummup, xxdxxdb_tamtap 
+    INFO: 
+    ****** Direct detection [cm^2]:  
+    INFO: SigmaN_SI_p         All DM = 2.05e-40       EXCLUDED  	LZ2024 ul        = 6.48e-47 
+    INFO: SigmaN_SI_n         All DM = 2.06e-40       EXCLUDED  	LZ2024 ul        = 6.48e-47 
+    INFO: SigmaN_SD_p         All DM = 2.58e-60       ALLOWED   	Pico60 (2019) ul = 4.16e-41 
+    INFO: SigmaN_SD_n         All DM = 1.01e-59       ALLOWED   	LZ2024 ul        = 7.59e-42 
+    INFO: 
+    ****** Indirect detection: 
+    INFO: <sigma v> method: inclusive  
+    INFO: ====== continuum spectrum final states 
+    INFO: DM particle halo velocity: 2e-05 c  
+    INFO: *** Print <sigma v> [cm^3 s^-1] with Fermi dSph limits 
+    INFO: xxdxxdb_ddx         All DM = 3.48e-29       ALLOWED   	Fermi dSph ul    = 3.30e-27 
+    INFO: xxdxxdb_uux         All DM = 3.48e-29       ALLOWED   	Fermi dSph ul    = 3.31e-27 
+    INFO: xxdxxdb_ssx         All DM = 3.48e-29       ALLOWED   	Fermi dSph ul    = 3.30e-27 
+    INFO: xxdxxdb_ccx         All DM = 3.48e-29       ALLOWED   	Fermi dSph ul    = 4.54e-27 
+    INFO: xxdxxdb_bbx         All DM = 3.41e-29       ALLOWED   	Fermi dSph ul    = 5.98e-27 
+    INFO: Skipping zero cross section processes for: xxdxxdb_y1y1, xxdxxdb_ttx, xxdxxdb_emep 
+    INFO: Using generic Fermi dSph limits for light quarks (u,d,s) 
+    INFO: DM DM > SM SM       All DM = 1.73e-28       ALLOWED   	Fermi dSph ul    = 2.96e-27 
+    INFO:  
+    INFO: *** Fluxes at earth [particle cm^-2 sr^-1]: 
+    INFO: gammas Flux         =	5.67e-12             
+    INFO: neutrinos_e Flux    =	2.71e-12             
+    INFO: neutrinos_mu Flux   =	5.00e-12             
+    INFO: neutrinos_tau Flux  =	4.26e-15             
+    INFO:  
+    INFO:  
+    INFO: Results written in: /root/DMspin1/output/run_01/MadDM_results.txt
+
+The results will be stored in the ``MadDM_results.txt`` file located in the process folder (in this case ``DMspin1/output/run_01/MadDM_results.txt``).
+In the process folder, you will also find additional files produced by the different MadDM modules. See the tutorials for those modules for additional details.
 
 Exiting MadDM
 =============
